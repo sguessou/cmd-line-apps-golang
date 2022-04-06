ecb32274563325b1cf45be56fe8ea00896dc1f1d
Saad Guessous <guessous_saad@hotmail.com>
2022-04-06 22:07:24 +0300

    Add counting number of bytes option

--- firstProgram/wc/main.go
+++ firstProgram/wc/main.go
@@ -10,16 +10,21 @@ import (
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	countBytes := flag.Bool("b", false, "Count number of bytes")
+
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *countBytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countWords bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
 
-	if !countLines {
+	switch {
+	case countWords:
 		scanner.Split(bufio.ScanWords)
+	case countBytes:
+		scanner.Split(bufio.ScanRunes)
 	}
 
 	wc := 0
--- firstProgram/wc/main_test.go
+++ firstProgram/wc/main_test.go
@@ -10,7 +10,7 @@ func TestCountWords(t *testing.T) {
 
 	exp := 4
 
-	res := count(b, false)
+	res := count(b, true, false)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
@@ -22,7 +22,19 @@ func TestCountLines(t *testing.T) {
 
 	exp := 3
 
-	res := count(b, true)
+	res := count(b, false, false)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("word1")
+
+	exp := 5
+
+	res := count(b, false, true)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
