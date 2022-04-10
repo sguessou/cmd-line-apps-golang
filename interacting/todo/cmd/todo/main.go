720ff66d8f26d8022d5dfd113c982d3bbc7f4123
Saad Guessous <guessous_saad@hotmail.com>
2022-04-10 16:09:00 +0300

    Display command-line tool usage

--- interacting/todo/cmd/todo/main.go
+++ interacting/todo/cmd/todo/main.go
@@ -11,6 +11,13 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s tool. Developed for sguessou inc\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2022\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage information:\n")
+		flag.PrintDefaults()
+	}
+
 	task := flag.String("task", "", "Task to be included in the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
