1084c90d863e960ba2b730000e2526022ad043be
Saad Guessous <guessous_saad@hotmail.com>
2022-04-18 19:36:37 +0300

    Add delete option

--- interacting/todo/cmd/todo/main.go
+++ interacting/todo/cmd/todo/main.go
@@ -23,6 +23,7 @@ func main() {
 
 	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
+	del := flag.Int("del", 0, "Item to be deleted")
 	complete := flag.Int("complete", 0, "Item to be completed")
 
 	flag.Parse()
@@ -43,6 +44,15 @@ func main() {
 		fmt.Print(l)
 	case *complete > 0:
 		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *del > 0:
+		if err := l.Delete(*del); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
