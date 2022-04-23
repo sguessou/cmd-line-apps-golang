64a4f0bd69ad608c2251e202975d98ae1db5739e
Saad Guessous <guessous_saad@hotmail.com>
2022-04-23 23:29:04 +0300

    Add tests for markdown preview tool

---
+++ workingFiles/mdp/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+)
+
+const (
+	inputFile  = "./testdata/test1.md"
+	resultFile = "test1.md.html"
+	goldenFile = "./testdata/test1.md.html"
+)
+
+func TestParseContent(t *testing.T) {
+	input, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result := parseContent(input)
+
+	expected, err := ioutil.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(expected, result) {
+		t.Logf("golden:\n%s\n", expected)
+		t.Logf("result:\n%s\n", result)
+		t.Error("Result content does not match golden file")
+	}
+
+}
---
+++ workingFiles/mdp/testdata/test1.md
@@ -0,0 +1,12 @@
+# Test Markdown file
+
+Just a test
+
+## Bullets:
+* Links [Link1](https://example.com)
+
+## Code Block
+```
+some code 
+```
+
