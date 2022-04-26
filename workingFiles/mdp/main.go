3210ff3d53962e8bbff1b66e9f1dc7d9dbc34563
Saad Guessous <guessous_saad@hotmail.com>
2022-04-26 19:35:54 +0300

    Add bash script for automatic preview when file changes

--- workingFiles/mdp/main.go
+++ workingFiles/mdp/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"time"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -72,6 +73,8 @@ func run(filename string, out io.Writer, skipPreview bool) error {
 		return nil
 	}
 
+	defer os.Remove(outName)
+
 	return preview(outName)
 }
 
@@ -114,5 +117,7 @@ func preview(fname string) error {
 		return err
 	}
 
-	return exec.Command(cPath, cParams...).Run()
+	err = exec.Command(cPath, cParams...).Run()
+	time.Sleep(2 * time.Second)
+	return err
 }
